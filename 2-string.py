411284a97ba1c565a1b403214fb7efd19060956e
Nasim09 <nasimlawrence@The-Hidden-Genius-Project-30.local>
2022-11-30 18:15:50 -0800

    nasim

---
+++ 2-string.py
@@ -0,0 +1,3 @@
+#print() - Print Function
+#String Concatenation - combine 2 or more strings together with (+) sign
+print ("Hello Nasim"  + " Lawrence")
---
+++ 3-input.py
@@ -0,0 +1,5 @@
+#print() - Print Function Prints out information to console
+#String Concatenation - combine 2 or more strings together with (+) sign
+#input() - Function get information from user
+print(input("What is your name?"))
+print("Hello" + " Nasim")
